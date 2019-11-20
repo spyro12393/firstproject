740e608ea6df1698dc87ec310e9b31c0947eac55
JJ Yeh <spyro12393@gmail.com>
2019-11-20 11:24:28 +0800

    <feat>: Done testing. p3-5.

---
+++ myapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from datetime import datetime
+
+# Create your views here.
+def sayhello(request, username):
+    now = datetime.now()
+    return render(request, "hello3.html", locals())
+
+def hello4(request, username):
+    now = datetime.now()
+    return render(request, "hello4.html", locals())
+
+def jquerytest(request):
+    return render(request, 'jquerytest.html', locals())
